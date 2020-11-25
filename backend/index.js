71950d8e5ebeb1c665a7c0a4b8496655774a599d
Joppe Rabijns <joppe.rabijns@student.ehb.be>
2020-11-25 11:28:43 +0100

    port fix

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 //express
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = (process.env.PORT || 3000);
 const apiRouter = express.Router();
 const cors = require('cors');
 const bodyParser = require('body-parser');
