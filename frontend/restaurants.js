ee8e68f13030b09d03af26853d5c18c5d08e2820
Joppe Rabijns <joppe.rabijns@student.ehb.be>
2020-11-25 12:00:43 +0100

    test1001

--- frontend/restaurants.js
+++ frontend/restaurants.js
@@ -11,46 +11,49 @@ var map = new mapboxgl.Map({
 fetchData();
 
 async function fetchData() {
-    let response = await fetch('https://cors-anywhere.herokuapp.com/https://web2-course-project-api-jopper.herokuapp.com/api/restaurants);
-      let data = await response.json(); allData(data); createMarkers(data); createRestaurantList(data);
-    }
+  let response = await fetch('https://cors-anywhere.herokuapp.com/https://web2-course-project-api-jopper.herokuapp.com/api/restaurants');
+  let data = await response.json();
+  allData(data);
+  createMarkers(data);
+  createRestaurantList(data);
+}
 
-    function allData(data) {
-      for (let key in data) {
-        const dataDiv = document.getElementById('allData');
-        let restaurantData = dataDiv.appendChild(document.createElement('h6'));
-        restaurantData.innerHTML = JSON.stringify(data[key]);
-      }
-    }
+function allData(data) {
+  for (let key in data) {
+    const dataDiv = document.getElementById('allData');
+    let restaurantData = dataDiv.appendChild(document.createElement('h6'));
+    restaurantData.innerHTML = JSON.stringify(data[key]);
+  }
+}
 
-    function createMarkers(data) {
-      for (let key in data) {
-        if (data.hasOwnProperty(key)) {
-          let logo = document.createElement('div');
-          logo.className = 'marker';
-          let marker = new mapboxgl.Marker(logo, {
-              anchor: 'bottom'
-            })
-            .setLngLat([data[key].geometry.coordinates[1], data[key].geometry.coordinates[0]])
-            .addTo(map);
-        }
-      }
+function createMarkers(data) {
+  for (let key in data) {
+    if (data.hasOwnProperty(key)) {
+      let logo = document.createElement('div');
+      logo.className = 'marker';
+      let marker = new mapboxgl.Marker(logo, {
+          anchor: 'bottom'
+        })
+        .setLngLat([data[key].geometry.coordinates[1], data[key].geometry.coordinates[0]])
+        .addTo(map);
     }
+  }
+}
 
-    function createRestaurantList(data) {
-      for (let key in data) {
-        let restaurantData = data[key].properties;
-        let restaurants = document.getElementById('restaurants');
-        let restaurant = restaurants.appendChild(document.createElement('div'));
-        restaurant.className = 'restaurantStyle';
-        let link = restaurant.appendChild(document.createElement('a'));
-        link.className = 'title';
-        link.innerHTML = restaurantData.name;
-        let details = restaurant.appendChild(document.createElement('h4'));
-        details.className = 'description';
-        details.innerHTML = restaurantData.description;
-        let address = restaurant.appendChild(document.createElement('h6'));
-        address.className = 'address';
-        address.innerHTML = restaurantData.address;
-      }
-    }
+function createRestaurantList(data) {
+  for (let key in data) {
+    let restaurantData = data[key].properties;
+    let restaurants = document.getElementById('restaurants');
+    let restaurant = restaurants.appendChild(document.createElement('div'));
+    restaurant.className = 'restaurantStyle';
+    let link = restaurant.appendChild(document.createElement('a'));
+    link.className = 'title';
+    link.innerHTML = restaurantData.name;
+    let details = restaurant.appendChild(document.createElement('h4'));
+    details.className = 'description';
+    details.innerHTML = restaurantData.description;
+    let address = restaurant.appendChild(document.createElement('h6'));
+    address.className = 'address';
+    address.innerHTML = restaurantData.address;
+  }
+}
