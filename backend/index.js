edc6981196504bb00b6aeb2a2bcf83009f9011be
Joppe Rabijns <joppe.rabijns@student.ehb.be>
2020-11-25 11:54:13 +0100

    cors fixes

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,13 @@ const client = new MongoClient(uri, {
   useNewUrlParser: true
 });
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 //middleware
 app.use(bodyParser.urlencoded({
