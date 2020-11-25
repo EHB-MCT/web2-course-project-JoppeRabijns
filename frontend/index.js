1d808e59c18c4361b6365684e105a5aa416d1be0
Joppe Rabijns <joppe.rabijns@student.ehb.be>
2020-11-25 09:45:21 +0100

    remove gitignore files

--- frontend/index.js
+++ frontend/index.js
@@ -1,59 +1,35 @@
 'use strict';
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoiam9wcGVyYWJpam5zIiwiYSI6ImNqNnl0dWxpNDJmeDEyeG10cWV0cnloczQifQ.YSoKiZUOwUPNMWbSPHBFbA';
-var map = new mapboxgl.Map({
-  container: 'map',
-  center: [4.7005176, 50.8798438],
-  zoom: 10,
-  style: 'mapbox://styles/mapbox/light-v10'
+document.getElementById("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  let search = document.getElementById("formSearchbar").value;
+  fetchData(search);
 });
 
-fetchData();
 
-async function fetchData() {
-  let response = await fetch('http://localhost:3000/api/restaurants');
+async function fetchData(search) {
+  let response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=cee7a81c28e441efa3b14a67c611c790&intolerances=gluten&number=5&query=${search}`);
   let data = await response.json();
-  allData(data);
-  createMarkers(data);
-  createRestaurantList(data);
+  addRecipe(data);
 }
 
-function allData(data) {
-  for (let key in data) {
-    const dataDiv = document.getElementById('allData');
-    let restaurantData = dataDiv.appendChild(document.createElement('h6'));
-    restaurantData.innerHTML = JSON.stringify(data[key]);
-  }
-}
 
-function createMarkers(data) {
-  for (let key in data) {
-    if (data.hasOwnProperty(key)) {
-      let logo = document.createElement('div');
-      logo.className = 'marker';
-      let marker = new mapboxgl.Marker(logo, {
-          anchor: 'bottom'
-        })
-        .setLngLat([data[key].geometry.coordinates[1], data[key].geometry.coordinates[0]])
-        .addTo(map);
-    }
-  }
-}
 
-function createRestaurantList(data) {
-  for (let key in data) {
-    let restaurantData = data[key].properties;
-    let restaurants = document.getElementById('restaurants');
-    let restaurant = restaurants.appendChild(document.createElement('div'));
-    restaurant.className = 'restaurantStyle';
-    let link = restaurant.appendChild(document.createElement('a'));
-    link.className = 'title';
-    link.innerHTML = restaurantData.name;
-    let details = restaurant.appendChild(document.createElement('h4'));
-    details.className = 'description';
-    details.innerHTML = restaurantData.description;
-    let address = restaurant.appendChild(document.createElement('h6'));
-    address.className = 'address';
-    address.innerHTML = restaurantData.address;
+function addRecipe(data) {
+  console.log(data);
+  let HTML = "";
+  for (let key in data.results) {
+    HTML += `<div id="card">
+  <div id="cardImageDiv">
+      <div id="cardInfoStars">
+          <h6 id="cardInfoText">4.0</h6>
+      </div>
+      <img id="cardImage" src="${data.results[key].image}" alt="">
+  </div>
+  <h2 id="cardTitle">${data.results[key].title}</h2>
+  <button class="cardButton" id="${data.results[key].id}">Bekijk</button>
+</div>`;
   }
+  console.log(HTML);
+  document.getElementById("cards").innerHTML = HTML;
 }
